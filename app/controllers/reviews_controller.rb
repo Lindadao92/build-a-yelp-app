bdb4c1ed6fe934416f011ef1e1fae31a0316570e
linda dao <lindadao92@gmail.com>
2021-10-06 22:58:02 +0200

    added delete reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,25 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, except: [:destroy]
+
   def new
-    set_restaurant
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
     # we need `restaurant_id` to associate review with corresponding restaurant
-    set_restaurant
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
       render :new
     end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
   end
 
   private
